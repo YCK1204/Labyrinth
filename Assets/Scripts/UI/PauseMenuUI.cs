d11456cad860f58a3f84f46e409125715d5d6e59
Minseok-creat <whalstjr3855@gmail.com>
2025-09-03 20:40:03 +0900

    [feat] UI 프리펩화

--- Assets/Scripts/UI/PauseMenuUI.cs
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 
 public class PauseMenuUI : MonoBehaviour
 {
-    public GameObject pauseMenuUI;
-    public GameObject exitUI;
-    public GameObject settingUI;
-
     public TextMeshProUGUI atkCountText;
     public TextMeshProUGUI atkSpdCountText;
     public TextMeshProUGUI defCountText;
@@ -39,5 +35,20 @@ public class PauseMenuUI : MonoBehaviour
     public void OnSettingButtonClicked()
     {
         Manager.UI.ShowSettingUI();
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Manager.UI.ShowExitUI();
+    }
+
+    private void UpdatePlayerStats()
+    {
+        if (_playerDataUI == null) return;
+
+        atkCountText.text = _playerDataUI.AttackPoint.ToString();
+        atkSpdCountText.text = _playerDataUI.AttackSpeed.ToString();
+        defCountText.text = _playerDataUI.DefensePoint.ToString();
+        hpCountText.text = _playerDataUI.CurrentHP.ToString();
     }
 }
