d6fa543081c840d5bf387fd6040b0ae8f4621269
Minseok-creat <whalstjr3855@gmail.com>
2025-09-04 15:42:06 +0900

    [feat] 로비 씬

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -28,6 +29,7 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         Manager.UI = this;
+        SetSceneUI(SceneManager.GetActiveScene().name);
     }
 
     public void SetSceneUI(string sceneName)
