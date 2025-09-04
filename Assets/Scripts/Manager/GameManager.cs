e53272892a99a9e884b6113aea393c3c3db4de2e
Kazin7 <krustm8@naver.com>
2025-09-04 14:55:44 +0900

    [Fix] 게임매니저 씬넘어가도 유지

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,9 +8,33 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        var existing = FindObjectsOfType<GameManager>();
+        if (existing.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
         if (_pc == null)
             _pc = FindObjectOfType<PlayerController>();
 
         Manager.Game = this;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (_pc == null)
+            _pc = FindObjectOfType<PlayerController>();
     }
 }
