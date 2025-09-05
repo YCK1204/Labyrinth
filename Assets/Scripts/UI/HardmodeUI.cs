9c2b4cf638aa95de0abccbc4edb001e756617201
Kazin7 <krustm8@naver.com>
2025-09-05 16:54:29 +0900

    [Fix] 던전 클리어시 하드모드 UI표시

--- Assets/Scripts/UI/HardmodeUI.cs
+++ Assets/Scripts/UI/HardmodeUI.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HardmodeUI : MonoBehaviour
 {
     [SerializeField] private GameObject hardModePanel;
 
-    void Start()
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        UpdateUI();
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         UpdateUI();
     }
@@ -12,11 +24,6 @@ public class HardmodeUI : MonoBehaviour
     void UpdateUI()
     {
         if (hardModePanel != null)
-        {
-            if (Manager.Game.PlayerData.IsHardMode)
-                hardModePanel.SetActive(true);
-            else
-                hardModePanel.SetActive(false);
-        }
+            hardModePanel.SetActive(Manager.Game.PlayerData.IsHardMode);
     }
 }
